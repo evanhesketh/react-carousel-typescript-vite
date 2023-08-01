804866cefb03f395bac77b2f7e0f3592e342f00c
Evan Hesketh <evanhesketh@yahoo.ca>
2023-08-01 16:30:43 -0400

    configure istanbul coverage reporter

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,16 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: './src/setupTests.ts',
-        // coverage: {
-        //   reporter: ['text', 'html'],
-        //   exclude: [
-        //     'node_modules/',
-        //     'src/setupTests.ts',
-        //   ],
-        // },
-      },
-})
-
-
-// /// <reference types="vitest" />
-// import { defineConfig } from 'vite'
-
-// export default defineConfig({
-//   test: {
-//     globals: true,
-//     environment: 'jsdom',
-//     setupFiles: './src/setupTests.ts',
-//     coverage: {
-//       reporter: ['text', 'html'],
-//       exclude: [
-//         'node_modules/',
-//         'src/setupTests.ts',
-//       ],
-//     },
-//   },
-// });
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    coverage: {
+      provider: "istanbul",
+    },
+  },
+});
